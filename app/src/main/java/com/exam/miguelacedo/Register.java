a57259c910271d00b02516d39e5ea83efc61e737
Chettyninho <acedomarfil1a@gmail.com>
2023-12-11 12:17:34 +0100

    toast implementados

--- app/src/main/java/com/exam/miguelacedo/Register.java
+++ app/src/main/java/com/exam/miguelacedo/Register.java
@@ -3,19 +3,24 @@ package com.exam.miguelacedo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Register extends AppCompatActivity {
 ImageView logo;
+Button registrarse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
+        registrarse = findViewById(R.id.backToLoginButton);
         logo = findViewById(R.id.mainImage);
 
         ImageView background = findViewById(R.id.background);
@@ -24,5 +29,13 @@ ImageView logo;
                 .transition(DrawableTransitionOptions.withCrossFade(100))
                 .fitCenter() // o centerCorp() para que se ponga en el centro de la img
                 .into(background);
+
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Usuario Registrado", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
